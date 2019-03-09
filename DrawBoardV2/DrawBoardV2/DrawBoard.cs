a3afc429ab5f6705ac63e8466454c1d29ebd0bb2
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-09 00:27:59 -0500

    DrawBoard Challenge complete

---
+++ DrawBoardV2/DrawBoardV2/DrawBoard.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawBoardV2
+{
+    public class DrawBoard
+    {
+        int width;
+        int height;
+        public DrawBoard(int w, int h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+
+        // prints rows of boxes for a specified width
+        public void DrawRows(int width)
+        {
+            int n = (2 * width) - 1;
+
+            // start i from 0 as 0 % 2 is 0 which is even
+            for (var i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("|");
+                }
+
+            }
+            Console.WriteLine();
+
+
+        }
+
+        // prints separator for a specified width not height
+        public void DrawSeparator(int width)
+        {
+            int n = (2 * width) - 1;
+
+            for (var i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("-");
+                }
+                else
+                {
+                    Console.Write("+");
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ DrawBoardV2/DrawBoardV2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawBoardV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter height of board between 2 and 30: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width of board between 2 and 30");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            if (height < 2 || height > 30)
+            {
+                Console.WriteLine("Please enter a valid height");
+            }
+            if(width < 2 || width > 30)
+            {
+                Console.WriteLine("Enter width of the board:");
+            }
+
+            //DrawBoard(width, height);
+
+            DrawBoard myBoard = new DrawBoard(width, height);
+            // call each function DrawRows and DrawSeparator for a specified height.
+            for (var i = 0; i < (2 * height) - 1; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    myBoard.DrawRows(width);
+                }
+                else
+                {
+                    myBoard.DrawSeparator(width);
+                }
+            }
+            Console.WriteLine();
+
+
+
+
+
+        }
+       
+        
+    }
+}
