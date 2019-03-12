ac3eb4e8d10cdc1a20c44552107fab9dbc2bf136
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-11 21:50:56 -0400

    Linked list practice

---
+++ removeKFromLinkedList/removeKFromLinkedList/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace removeKFromLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = new List<int> { 3, 1, 2, 3, 4, 5 };
+            var l = CreateLinkedList(input);
+            Console.WriteLine(l.value);
+            Console.WriteLine(l.next.value);
+            Console.WriteLine(l.next.next.value);
+
+        }
+
+        static ListNode<int> CreateLinkedList(List<int> input)
+        {
+            ListNode<int> head = new ListNode<int>();
+
+            var currNode = head;
+            for (var i = 0; i < input.Count; i++)
+            {
+                currNode.value = input[i];
+                if (i < input.Count - 1)
+                {
+                    currNode.next = new ListNode<int>();
+                    currNode = currNode.next;
+                }
+            }
+
+            return head;
+
+        }
+
+        static void printLinkedList(ListNode<int> l)
+        {
+
+        }
+        static ListNode<int> removeKFromList(ListNode<int> l, int k)
+        {
+
+            ListNode<int> traveller = l;
+
+            if (l == null)
+            {
+                return l;
+            }
+
+
+            return l;
+        }
+
+
+
+        //Definition for singly-linked list:
+        class ListNode<T>
+        {
+            public T value { get; set; }
+            
+            public ListNode<T> next { get; set; }
+        }
+
+        
+
+    }
+}
