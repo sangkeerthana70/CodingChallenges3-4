06dd5194204685cb17049e3b0f74fe142cd1dbcc
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-14 23:50:36 -0400

    century from year complete

---
+++ CodeSignal/centuryFromYear/centuryFromYear/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace centuryFromYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 1988;
+            
+            centuryFromYear(year);
+           
+        }
+
+        public static int centuryFromYear(int year)
+        {
+                 
+            int century = 0;
+            if(year % 100 == 0)
+            {
+                century = year / 100;
+                Console.WriteLine("a: " + century);
+            }
+            else
+            {
+                century = (year / 100) + 1;
+                Console.WriteLine("century: " + century);
+            }
+
+            return century;
+        }
+
+       
+    }
+}        
+
+
+    
+
