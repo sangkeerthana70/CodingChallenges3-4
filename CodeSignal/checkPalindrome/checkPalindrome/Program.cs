21e5952f421e0f5cddb5f1acee3ebef7306fe63d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-14 23:59:33 -0400

    initial commit

---
+++ CodeSignal/checkPalindrome/checkPalindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "aabaa";
+        }
+
+        public static bool checkPalindrome(string inputString)
+        {
+
+            char[] arr = inputString.ToCharArray();
+            foreach(char item in arr)
+            {
+                Console.WriteLine("char: " + item);
+            }
+            Array.Reverse(arr);
+
+            //return new string(arr);
+            return true;
+        }
+
+    }
+}
