e059ba89aff33e3cf66b96af2f771f1f0756936d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-15 23:38:44 -0400

    find max product of two adjacent elements in an array

---
+++ CodeSignal/adjacentElementsProduct/adjacentElementsProduct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adjacentElementsProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] inputArray = new int[] { 4, 1, 2, 3, 1, 5 };
+            int[] inputArray = new int[] { -23, 4, -3, 8, -12 };
+            int result = adjacentElementsProduct(inputArray);
+            Console.WriteLine("result: " + result);
+        }
+
+        public static int adjacentElementsProduct(int[] inputArray)
+        {
+
+            int product = int.MinValue;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if (inputArray[i] * inputArray[i + 1] > product)
+                {
+                    product = inputArray[i] * inputArray[i + 1];
+                    //Console.WriteLine("product in loop: " + product);
+                }
+                
+            }
+            return product;
+        }
+
+    }
+}
