6f00078b99e3511cf9fa03259dcba17cd97a143d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-15 21:56:12 -0400

    w.i.p

--- CodeSignal/checkPalindrome/checkPalindrome/Program.cs
+++ CodeSignal/checkPalindrome/checkPalindrome/Program.cs
@@ -11,20 +11,44 @@ namespace checkPalindrome
         static void Main(string[] args)
         {
             string input = "aabaa";
+            bool result = checkPalindrome(input);
+            Console.WriteLine("result: " + result);
         }
 
         public static bool checkPalindrome(string inputString)
         {
-
+            bool res;
             char[] arr = inputString.ToCharArray();
             foreach(char item in arr)
             {
                 Console.WriteLine("char: " + item);
             }
-            Array.Reverse(arr);
 
-            //return new string(arr);
-            return true;
+            Console.WriteLine();
+             Array.Reverse(arr);
+            foreach (char reverse in arr)
+            {
+                Console.WriteLine("reverse: " + reverse);
+            }
+            string s1 = String.Join(" ", arr);
+            Console.WriteLine("s1: " + s1);
+
+            Console.WriteLine(String.Equals(inputString, s1));
+            /*
+            if (myOutput == 0)
+            {
+                //Console.WriteLine(inputString.Equals(s1));
+                res = true;
+                
+            }
+            else
+            {
+                Console.WriteLine("In else");
+                Console.WriteLine(inputString.Equals(s1));
+                res = false;
+            }*/
+            
+            return String.Equals(inputString, s1);
         }
 
     }
