e9c5c56c8790bc3c8eface0ffb9ddd8b7e891be7
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-03-24 12:33:23 -0400

    2/5 test cases passed

---
+++ CompanyChallenges/Uber/perfectCity/perfectCity/City.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfectCity
+{
+    public class City
+    {
+        double[] departure { get; set; }
+        double[] destination { get; set; }
+
+        public City(double []depart, double[] destiny)
+        {
+            this.departure = depart;
+            this.destination = destiny;
+        }
+        public double perfectCity(City data)
+        {
+            double shortestTime = 0;
+            double te = 0;
+            double temp = 0;
+            double temp1 = 0;
+            double temp2 = 0;
+            for (int i = 0; i < departure.Length - 1; i++)
+            {
+                Console.WriteLine("Ceiling: " + Math.Ceiling(departure[i]));
+                if (departure[i] < Math.Ceiling(departure[i]))
+                {
+                    temp = Math.Abs(departure[i] - Math.Ceiling(departure[i]));
+                    Console.WriteLine("temp: " + temp);
+                }
+                //te = Math.Abs(temp + destination[i]);
+                //Console.WriteLine("te: " + te);
+                temp1 = Math.Abs(departure[i + 1] - destination[i + 1]);
+                Console.WriteLine("temp1: " + temp1);
+                temp2 = Math.Abs(destination[i] - Math.Round(destination[i]));
+                Console.WriteLine("temp2 " + temp2);
+                shortestTime = temp + temp1 + temp2;
+                Console.WriteLine(shortestTime);
+            }
+            return shortestTime;
+        }
+
+    }
+    
+}
--- CompanyChallenges/Uber/perfectCity/perfectCity/Program.cs
+++ CompanyChallenges/Uber/perfectCity/perfectCity/Program.cs
@@ -10,6 +10,17 @@ namespace perfectCity
     {
         static void Main(string[] args)
         {
+            //double[] departure = { 0.4, 1 };
+            //double[] destination = { 0.9, 3 };
+            // expected output = 2.7;
+
+            double[] departure = { 0.9, 6 };
+            double[] destination = { 1.1, 5 };
+            // expected output = 1.2;
+
+            City myCity = new City(departure, destination);
+            double result = myCity.perfectCity(myCity);
+            Console.WriteLine("result: " + result);
         }
     }
 }
