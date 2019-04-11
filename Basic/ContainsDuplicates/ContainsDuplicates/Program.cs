d7b9681fbba1e2b381826b32cc5ad66c49a5672d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-11 13:25:16 -0400

    Checking for duplicate elements in an array

---
+++ Basic/ContainsDuplicates/ContainsDuplicates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainsDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[] { 1, 2, 3, 4 };
+            bool result = containsDuplicates(arr);
+            Console.WriteLine(result);
+        }
+
+        public static bool containsDuplicates(int[] a)
+        {
+            bool result = false;
+            Array.Sort(a);
+            for(int i = 0; i < a.Length -1; i++)
+            {
+                if (a[i] == a[i + 1])
+                    return true;
+            }        
+            return false;
+        }
+
+    }
+}
