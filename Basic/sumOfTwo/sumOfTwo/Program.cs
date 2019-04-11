2122a6daa03440f62b6f24ff404b2ababa999eba
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-11 13:40:26 -0400

    sumOfTwo solved

---
+++ Basic/sumOfTwo/sumOfTwo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2, 3 };
+            int[] b = new int[] { 10, 20, 30, 40 };
+            int value = 42;
+            bool result = sumOfTwo(a, b, value);
+            Console.WriteLine(result);
+
+        }
+
+        public static bool sumOfTwo(int[] a, int[] b, int v)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                for(int j = 0; j < b.Length; j++)
+                {
+                    if(a[i] + b[j] == v)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
