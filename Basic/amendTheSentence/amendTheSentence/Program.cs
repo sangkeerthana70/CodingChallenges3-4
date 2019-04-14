edd1e0eeec72589e11744485ed67bf10d78ff006
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-14 19:58:37 -0400

    Initial commit

---
+++ Basic/amendTheSentence/amendTheSentence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amendTheSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "CodesignalIsAwesome";
+            string result = amendTheSentence(s);
+            Console.WriteLine("result: " + result);
+
+        }
+        public static string amendTheSentence(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char letter in s)
+            {
+                if (char.IsUpper(letter))
+                {
+                    sb.Append(' ');
+                }
+                sb.Append(letter);
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
