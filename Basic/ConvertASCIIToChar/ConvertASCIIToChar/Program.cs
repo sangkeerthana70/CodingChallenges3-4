5bf49d6c983d0dc62933c4b87824bd6d1136a7e2
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-13 23:42:09 -0400

    Initial commit

---
+++ Basic/ConvertASCIIToChar/ConvertASCIIToChar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertASCIIToChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int unicode = 65;
+            char character = (char)unicode;
+            Console.WriteLine(character);
+            string text = character.ToString();
+            Console.WriteLine(text);
+        }
+    }
+}
