f2ba0a16685563b13eb4dc2ef3d7ec6c028227a5
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-15 14:56:06 -0400

    Initial commit

---
+++ CodeSignal/shapeArea/shapeArea/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int result = shapeArea(n);
+            
+            Console.WriteLine("result : " + result);
+        }
+
+        public static int shapeArea(int n)
+        {
+            
+            return n;
+            
+
+        }
+
+    }
+}
