cb1e62d3d2515fd59594c5dabf852d8ece74fa8f
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-16 23:46:39 -0400

    Challenge solved

--- CodeSignalArcade/MakeArrayConsecutive2/MakeArrayConsecutive2/Program.cs
+++ CodeSignalArcade/MakeArrayConsecutive2/MakeArrayConsecutive2/Program.cs
@@ -18,29 +18,19 @@ namespace MakeArrayConsecutive2
         }
 
         public static int makeArrayConsecutive2(int[] statues)
-        {
-            
+        {           
             Array.Sort(statues);
-
-            Console.WriteLine(String.Join(",", statues));
+            //Console.WriteLine(String.Join(",", statues));
             int statuesNeeded = 0;
             for (int i = 0; i < statues.Length - 1; i++)
             {
                 
-                Console.WriteLine("i is: " + i);
-                
                 if(statues[i] + 1 != statues[i + 1])
                 {
-                    Console.WriteLine("inside if");
-                    Console.WriteLine("statues[i]: " + statues[i]);
-                    Console.WriteLine("statues[i+1]: " + statues[i + 1]);
-                    int difference = Math.Abs(statues[i] - statues[i + 1]);
-                    Console.WriteLine("difference: " + difference);
-                    statuesNeeded += Math.Abs(difference - 1);
-                    
+                    int difference = Math.Abs(statues[i] - statues[i + 1]);                   
+                    statuesNeeded += Math.Abs(difference - 1);                   
                 }
             }
-            Console.WriteLine("3: " + statuesNeeded);
             return statuesNeeded;
         }
 
