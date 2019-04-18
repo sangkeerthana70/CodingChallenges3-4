7109d9d29395adf2448448a081fd12eb25ae9f24
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-17 21:40:16 -0400

    Initial commit

---
+++ DailyChallenge/KthDigit/KthDigit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KthDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        // Given an integer, find its kth digit.
+
+        public static int kthDigit(int n, int k)
+        {
+
+
+            return 0;
+        }
+
+    }
+}
