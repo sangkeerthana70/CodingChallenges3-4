3cf761be44d904098c011545a0df24e833d07786
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-17 22:37:14 -0400

    Working solution

--- DailyChallenge/KthDigit/KthDigit/Program.cs
+++ DailyChallenge/KthDigit/KthDigit/Program.cs
@@ -10,16 +10,34 @@ namespace KthDigit
     {
         static void Main(string[] args)
         {
-
+            int n = 578943;
+            int k = 2;
+            int result = kthDigit(n, k);
+            Console.WriteLine("result: " + result);
         }
 
         // Given an integer, find its kth digit.
 
         public static int kthDigit(int n, int k)
         {
+            int kDigit = 0;
+            char [] number = n.ToString().ToArray();
+            for(int i = 0; i < number.Length -1; i++)
+            {
+                Console.WriteLine("char in array: " + number[i]);
+                Console.WriteLine(k-1);
+                Console.WriteLine("k - 1: " + number[k-1]);
+                if(i == (k - 1))
+                {
+                    Console.WriteLine("Inside if");
+                    number[k - 1].ToString();
+                    kDigit = Convert.ToInt32(number[k - 1].ToString());
+                    Console.WriteLine("kDigit: " + kDigit);
+                }
+            }
+            return kDigit;
 
-
-            return 0;
+            
         }
 
     }
