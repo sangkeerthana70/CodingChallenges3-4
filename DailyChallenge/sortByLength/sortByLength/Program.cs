2a30ab5003a43b28399818a7ce223f294880bbda
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-22 23:43:14 -0400

    Initial commit on the algo

---
+++ DailyChallenge/sortByLength/sortByLength/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortByLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string []inputArray = {"abc",
+              "",
+              "aaa",
+              "a",
+              "zz" };
+            string[] result = sortByLength(inputArray);
+        }
+
+        static string[] sortByLength(string[] inputArray)
+        {
+            string[] result = new string[] { };
+
+            return  result;
+        }
+
+    }
+}
