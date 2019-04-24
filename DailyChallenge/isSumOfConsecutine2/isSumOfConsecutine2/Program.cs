dba51019c5ce4b450b35fa7236b6c20068727da8
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-24 12:00:58 -0400

    workingf solution

--- DailyChallenge/isSumOfConsecutine2/isSumOfConsecutine2/Program.cs
+++ DailyChallenge/isSumOfConsecutine2/isSumOfConsecutine2/Program.cs
@@ -10,16 +10,41 @@ namespace isSumOfConsecutine2
     {
         static void Main(string[] args)
         {
-            int n = 9;
+            int n = 15;
             int result = isSumOfConsecutive2(n);
             Console.WriteLine("result: " + result);
         }
+        /*
+         * Find the number of ways to express n as sum of some (at least two) 
+         * consecutive positive integers.
+        */
 
         static int isSumOfConsecutive2(int n)
         {
 
-            return 0;
-  
+            int combination = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int sum = 0;
+                Console.WriteLine("i is: " + i);
+                for (int j = i; j < n; j++)
+                {
+                    sum += j;
+                    if (sum == n)
+                    {
+                        Console.WriteLine("sum: " + sum);
+                        combination += 1;
+                        Console.WriteLine("combination: " + combination);
+                    }
+                    if (sum > n)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("sum: " + sum);
+            }
+            return combination;
+
         }
 
     }
