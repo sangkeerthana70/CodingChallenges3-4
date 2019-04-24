f2457147d57720d61cdead2aa5e5efd2383ffb9a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-23 23:23:39 -0400

    initial commit

---
+++ DailyChallenge/isSumOfConsecutine2/isSumOfConsecutine2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isSumOfConsecutine2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+            int result = isSumOfConsecutive2(n);
+            Console.WriteLine("result: " + result);
+        }
+
+        static int isSumOfConsecutive2(int n)
+        {
+
+            return 0;
+  
+        }
+
+    }
+}
