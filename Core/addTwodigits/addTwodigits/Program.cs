ae3c4f4ee5c79d6ae2d3d576920d331da01be875
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-25 12:15:25 -0400

    addTwoDigits of an number solution

---
+++ Core/addTwodigits/addTwodigits/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addTwodigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 99;
+            int result = addTwoDigits(n);
+            Console.WriteLine("result: " + result);
+        }
+        /*
+         * You are given a positive two-digit integer n < 99. Return the sum of its digits.
+         */
+
+        static int addTwoDigits(int n)
+        {
+
+            int lastDigit = n % 10;
+            //Console.WriteLine("remainder: " + lastDigit);
+            int firstDigit = n / 10;
+            //Console.WriteLine("quotient: " + firstDigit);
+            return lastDigit + firstDigit;
+
+        }
+
+    }
+}
