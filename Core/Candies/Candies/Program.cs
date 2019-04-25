db93326e3cee8a15bfd5beb75f77707f2f8a8548
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-25 13:05:14 -0400

    Solved candies

---
+++ Core/Candies/Candies/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int m = 10;
+            int result = candies(n, m);
+            Console.WriteLine("result: " + result);
+        }
+
+        /*
+         * n children have got m pieces of candy. They want to eat as much candy as they can, but each 
+         * child must eat exactly the same amount of candy as any other child. Determine how many pieces 
+         * of candy will be eaten by all the children together. Individual pieces of candy cannot be split.
+        */
+
+        static int candies(int n, int m)
+        {
+            //Math.round(Converts the values to a double data type and we have to explicitly convert values of m and n to a Decimal
+            //to then convert it to a Integer.
+            decimal candiesEaten = Math.Round(Convert.ToDecimal(m / n));
+            
+            Console.WriteLine(candiesEaten.GetType());
+            Console.WriteLine("decimal value: " + candiesEaten);
+           
+
+            return Convert.ToInt32(candiesEaten) * n;
+        }
+
+    }
+}
--- Core/largestNumber.cs
+++ Core/largestNumber.cs
@@ -1,3 +1,8 @@
+/*
+Given an integer n, return the largest number that contains exactly n digits.
+*/
+
+
 int largestNumber(int n) {
     Console.WriteLine(Convert.ToInt32(Math.Pow(10, n) -1));
     return Convert.ToInt32(Math.Pow(10, n) -1);
