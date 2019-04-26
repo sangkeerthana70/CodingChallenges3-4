954c779a70729a5acc4b11249b37a0274879d288
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-26 13:30:07 -0400

    Find the largest multiple for a given divisor and a bound such that it is divisible by the divisor and less than or equal to the bound

---
+++ Core/maxMultiple/maxMultiple/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int divisor = 9;
+            int bound = 60;
+            int result = maxMultiple(divisor, bound);
+            Console.WriteLine("result: " + result);
+
+        }
+        /*
+         * Given a divisor and a bound, find the largest integer N such that:
+
+           * N is divisible by divisor.
+           * N is less than or equal to bound.
+           * N is greater than 0
+         */
+        static int maxMultiple(int divisor, int bound)
+        {
+            int maxMultipleNumber = (bound / divisor) * divisor;           
+            return maxMultipleNumber;
+        }
+
+    }
+}
