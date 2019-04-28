5da8f42549a634ff73dc25b8d532ce067eba5af6
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-27 23:22:41 -0400

    Initial commit

---
+++ Core/circleOfNumbers/circleOfNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int firstNumber = 2;
+            int result = circleOfNumbers(n, firstNumber);
+            Console.WriteLine(result);
+
+        }
+        static int circleOfNumbers(int n, int firstNumber)
+        {
+          
+            int halfway = n/2;
+            int radiallyOppositeNumber = 0;
+            if(firstNumber < halfway)
+            {
+                radiallyOppositeNumber = firstNumber + halfway;
+                
+            }
+            else
+            {
+                radiallyOppositeNumber = firstNumber - halfway;
+                
+            }
+            
+            
+            return radiallyOppositeNumber;
+
+        }
+    }
+}
