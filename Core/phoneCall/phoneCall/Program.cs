f9c3e6b64372f45d19ebe6447909eeb83e53d079
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-28 23:39:27 -0400

    working solution

--- Core/phoneCall/phoneCall/Program.cs
+++ Core/phoneCall/phoneCall/Program.cs
@@ -10,10 +10,10 @@ namespace phoneCall
     {
         static void Main(string[] args)
         {
-            int s = 2;
-            int min1 = 2;
-            int min2_10 = 2;
-            int min11 = 1;
+            int s = 20;
+            int min1 = 3;
+            int min2_10 = 1;
+            int min11 = 2;
             int result = phoneCall(min1, min2_10, min11, s);
             Console.WriteLine("result: " + result);
         }
@@ -21,15 +21,49 @@ namespace phoneCall
         static int phoneCall(int min1, int min2_10, int min11, int s)
         {
 
-            int firstMinute = s - min1;
+            if (s < min1)
+            {
+                return 0;
+            }
+
+            int runningBalance = s;
+            int minutes = 0;
+
+            // first minute
+            runningBalance -= min1;
+            minutes += 1;
+
+            // 2 to 10 mins
+            if (runningBalance >= (min2_10 * 9))
+            {
+                runningBalance -= (min2_10 * 9);
+                minutes += 9;
+            }
+            else
+            {
+                minutes += runningBalance / min2_10;
+                return minutes;
+            }
+
+            // > 10 mins
+            minutes += runningBalance / min11;
+            return minutes;
+
+            /*
+            int firstMinute = Math.Abs(s - min1);
             Console.WriteLine("first: " + firstMinute);
-            int minutes2Through10 = min2_10 * 9;
-            Console.WriteLine("second: " + minutes2Through10);
-            int remainingMinutes = firstMinute - minutes2Through10;
-            Console.WriteLine("remainingminutes: " + remainingMinutes);
-            int minutes11AndUp = remainingMinutes / min11;
-            Console.WriteLine("last: " + minutes11AndUp);
-            return  1 + minutes2Through10 + minutes11AndUp;
+            int minutes2Through10 = Math.Abs(min2_10* 9);
+            Console.WriteLine("second : " + minutes2Through10);
+            int  minutes11= minutes2Through10 / min11;
+            Console.WriteLine("minutes11: " + minutes11);
+            int remainingMinutes = 1 + minutes2Through10 + minutes11;
+            if (remainingMinutes > s)
+            {
+                return 0;
+            }
+            return remainingMinutes;
+            */
+
         }
     }
 
