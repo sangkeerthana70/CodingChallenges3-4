239a328af964cae29883192bac306632301d671a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-01 14:38:51 -0400

    findEmailDomain Initial commit

---
+++ DailyChallenge/findEmailDomain/findEmailDomain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findEmailDomain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = "prettyandsimple@example.com";
+            string result = findEmailDomain(address);
+            Console.WriteLine("result " + result);
+        }
+
+        static string findEmailDomain(string address)
+        {
+            string[] strArr = address.Split('@');
+            int index = address.IndexOf('@');
+            int strlength = address.Length -1;
+            if (address.Contains('@')){
+                return address.Substring(index + 1);
+            }
+            return "";
+        }
+
+    }
+}
