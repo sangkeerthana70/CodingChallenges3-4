bba6f6193d3b41c9ad0ee92f23fe588fb7dcd0e0
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-01 22:28:07 -0400

    comments added

--- DailyChallenge/firstNotDivisible/firstNotDivisible/Program.cs
+++ DailyChallenge/firstNotDivisible/firstNotDivisible/Program.cs
@@ -15,12 +15,22 @@ namespace firstNotDivisible
             int result = firstNotDivisible(divisors, start);
             Console.WriteLine("result: " + result);
         }
+
+        /*
+         * Find the smallest integer not less than the given limit which is not divisible
+         * by any of the integers from the given array.
+
+        Example
+
+        For divisors = [2, 3, 4] and start = 14, the output should be
+        firstNotDivisible(divisors, start) = 17.
+        */
         static int firstNotDivisible(int[] divisors, int start)
         {
             
            while(start <= 150)
            {
-                Console.WriteLine("start: " + start);
+                //Console.WriteLine("start: " + start);
                 bool divisible = false;
                 for (int i = 0; i < divisors.Length; i++)
                 {
