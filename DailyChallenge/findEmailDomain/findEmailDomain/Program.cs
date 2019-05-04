3898f8787691eb04f63de779bbb15ae4cbf9673a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-03 22:19:34 -0400

    program to find email domain from a given string

--- DailyChallenge/findEmailDomain/findEmailDomain/Program.cs
+++ DailyChallenge/findEmailDomain/findEmailDomain/Program.cs
@@ -10,20 +10,33 @@ namespace findEmailDomain
     {
         static void Main(string[] args)
         {
-            string address = "prettyandsimple@example.com";
+            //string address = "prettyandsimple@example.com";
+            string address = "\"very.unusual.@.unusual.com\"@usual.com";
             string result = findEmailDomain(address);
             Console.WriteLine("result " + result);
         }
 
+        /*
+         * An email address such as "John.Smith@example.com" is made up of a local part ("John.Smith"),
+         * an "@" symbol, then a domain part ("example.com").
+
+          The domain name part of an email address may only consist of letters, digits, hyphens and dots.
+          The local part, however, also allows a lot of different special characters. 
+
+          Given a valid email address, find its domain part.
+        */
+
         static string findEmailDomain(string address)
         {
             string[] strArr = address.Split('@');
-            int index = address.IndexOf('@');
+            //int index = address.IndexOf('@');
+            int index = address.LastIndexOf('@');
+            string result = "";
             int strlength = address.Length -1;
             if (address.Contains('@')){
-                return address.Substring(index + 1);
+                result = address.Substring(index + 1);
             }
-            return "";
+            return result;
         }
 
     }
