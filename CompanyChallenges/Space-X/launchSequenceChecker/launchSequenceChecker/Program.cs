bbcefe2940c8f45da579b1f616e6324123017ff2
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-04 23:44:15 -0400

    Initial commit - launchSequenceChecker

---
+++ CompanyChallenges/Space-X/launchSequenceChecker/launchSequenceChecker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace launchSequenceChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] systemNames = { "stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon" };
+            int[] stepNumbers = { 1, 10, 11, 2, 12, 111 };
+            bool result = launchSequenceChecker(systemNames, stepNumbers);
+        }
+
+        static bool launchSequenceChecker(string[] systemNames, int[] stepNumbers)
+        {
+            Dictionary<string, List<int>> sequenceDict  = new Dictionary<string, List<int>>();
+            for(int i = 0; i < systemNames.Length; i++)
+            {
+                
+            }
+
+        }
+
+    }
+}
