90e0f76cc67b872eedcc1748747d5818fe989944
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-06 13:48:59 -0400

    Brute force approach

---
+++ CompanyChallenges/Space-X/CompanyChallengeLaunchSequence/CompanyChallengeLaunchSequence/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyChallengeLaunchSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] systemNames = { "Falcon 9", "Falcon 9", "Falcon 9", "Falcon 9", "Falcon 9", "Falcon 9" };
+            int[] stepNumbers = { 1, 3, 5, 7, 7, 9 };
+            bool result = launchSequenceChecker(systemNames, stepNumbers);
+            Console.WriteLine("result: " + result);
+        }
+
+        static bool launchSequenceChecker(string[] systemNames, int[] stepNumbers)
+        {
+
+           for(int i = 0; i < systemNames.Length; i++)
+           {
+                //Console.WriteLine("i: " + i);
+                //Console.WriteLine(systemNames[i]);
+                for (int j = i+ 1; j < systemNames.Length ; j++)
+                {
+                    //Console.WriteLine("j: " + j);
+                    if (systemNames[i] == systemNames[j])
+                    {
+                        
+                        
+                        //Console.WriteLine(systemNames[j]);
+                        //Console.WriteLine("match found");
+                        if(stepNumbers[i] >= stepNumbers[j])
+                        {
+                            
+                            return false;
+                        }                      
+                    }                  
+                }
+           }
+           return true;
+
+
+        }
+
+    }
+}
