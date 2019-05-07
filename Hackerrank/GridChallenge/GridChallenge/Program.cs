18d05df6d1a324d5de5d9555687a98965ff04433
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-07 13:31:48 -0400

    Initial commit

---
+++ Hackerrank/GridChallenge/GridChallenge/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                string[] grid = new string[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    string gridItem = Console.ReadLine();
+                    grid[i] = gridItem;
+                }
+
+                string result = gridChallenge(grid);
+
+            }
+            */
+            string[][] grid = new string[3][];
+            grid[0] = new string[] { "e", "b", "a" };
+            grid[1] = new string[] { "f", "g", "h" };
+            grid[2] = new string[] { "o", "l", "m" };
+
+
+
+        }
+        // Complete the gridChallenge function below.
+        static string gridChallenge(string[] grid)
+        {
+            for(int i = 0; i < grid.GetLength(0); i++)
+            {
+                for(int j = 0; j < grid.GetLength(1); j++)
+                {
+
+                }
+            }
+            return "";
+        }
+    }
+}
