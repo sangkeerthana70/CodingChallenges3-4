74c70f04b191e11340758b5a36370d3341c1d010
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-07 23:36:30 -0400

    created a custom function to find the index of a number

---
+++ CustomIndexOfFunctions/CustomIndexOfFunctions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomIndexOfFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 7, 25, 21, 3, 25, 7 };
+            int val = 0;
+            int result = FindIndexOfVal(arr, val);
+            Console.WriteLine("result: " + result);
+        }
+        static int FindIndexOfVal(int[] arr, int val)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == val)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
