07aff70084c83d584bd3f795f97417169396dd5d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-08 13:29:57 -0400

    Solved challenge

---
+++ DailyChallenge/percentageGreen/percentageGreen/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace percentageGreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int green = 2;
+            int total = 5;
+            percentageGreen(green, total);
+
+        }
+
+        static int percentageGreen(int green, int total)
+        {
+
+            
+
+            double percentage = ((double)green / total) * 100;
+
+            Console.WriteLine(percentage);
+            return (int)percentage;
+        }
+
+    }
+}
