af25a34332a330345a23db7a104a4fd9b01aaf9a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-08 13:45:12 -0400

    sOLVED cHALLENGE

---
+++ DailyChallenge/swapCase/swapCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Beautiful Day";
+            string result = swapCase(text);
+            Console.WriteLine("result: " + result);
+        }
+
+        static string swapCase(string text)
+        {
+            string result = "";
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                {
+                    result += char.ToLower(text[i]).ToString();
+                }
+                else
+                {
+                    result += char.ToUpper(text[i]).ToString();
+                }
+            }
+            return result;
+
+        }
+
+    }
+}
