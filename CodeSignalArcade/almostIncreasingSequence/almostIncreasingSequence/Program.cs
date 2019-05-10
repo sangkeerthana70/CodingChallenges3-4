f8da06cfd33869ce11c478038c0b7bd7d203b965
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-09 23:51:14 -0400

    Made few changes to declare a List before the loop

--- CodeSignalArcade/almostIncreasingSequence/almostIncreasingSequence/Program.cs
+++ CodeSignalArcade/almostIncreasingSequence/almostIncreasingSequence/Program.cs
@@ -10,32 +10,47 @@ namespace almostIncreasingSequence
     {
         static void Main(string[] args)
         {
-            int[] sequence = new int[] { 1, 3, 2, 1 };
+            int[] sequence = new int[] { 3, 6, 5, 8, 10, 20, 15 };
             bool result = almostIncreasingSequence(sequence);
             Console.WriteLine("result: " + result);
         }
 
+        
         public static bool almostIncreasingSequence(int[] sequence)
         {
-
-            //Array.Sort(sequence);
-            //Console.WriteLine(String.Join(",", sequence));
-            for(int i = 0; i < sequence.Length -1; i++)
+            //List<int> newSequence = new List<int>();
+            List<int> newSequence = sequence.ToList<int>();
+            Console.WriteLine(String.Join(",", newSequence));
+            for (int i = 0; i < newSequence.Count; i++)
             {
-                Console.WriteLine("i is: " + i);
-                if(sequence[i] > sequence[i + 1])
+                Console.WriteLine("i in List: " + i);
+                newSequence.RemoveAt(i);
+                Console.WriteLine("after removing element at " + i + ": " + String.Join(",", newSequence));
+                if (increasingSequence(newSequence))
                 {
-                    Console.WriteLine("sequence[i] : " + sequence[i]);
-                    Console.WriteLine("sequence[i+1]: " + sequence[i + 1]);
-                    int difference = Math.Abs(sequence[i] - sequence[i + 1]);
-                    Console.WriteLine("difference: " + difference);
-                    return false;
+                    return true;
                 }
             }
             
+            return false;
+        }
+
+        public static bool increasingSequence(List<int> sequence)
+        {
+            Console.WriteLine(String.Join(",", sequence));
+            for (int i = 0; i < sequence.Count - 1; i++)
+            {
+                Console.WriteLine("i in bool: " + i);
+                Console.WriteLine("sequence[i] : " + sequence[i]);
+                Console.WriteLine("sequence[i + 1] : " + sequence[i + 1]);
+                if (sequence[i] >= sequence[i + 1])
+                {
+                    Console.WriteLine("inside if");
+                    return false;
+                }
+            }
             return true;
         }
-        
 
     }
 
