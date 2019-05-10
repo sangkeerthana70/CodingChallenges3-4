b55af3c67d46df35313012e6300385379cd91f4b
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-09 23:46:10 -0400

    passed all test cases

--- DailyChallenge/replaceMiddle/replaceMiddle/Program.cs
+++ DailyChallenge/replaceMiddle/replaceMiddle/Program.cs
@@ -17,36 +17,31 @@ namespace replaceMiddle
 
         static int[] replaceMiddle(int[] arr)
         {
-            List<int> replaceMiddleElement = new List<int>(arr);
-            Console.WriteLine("ArrToList: " + String.Join(",", replaceMiddleElement));
-            float median = 0;
-            int arrLength = replaceMiddleElement.Count;
+            List<int> arrToList = new List<int>(arr);
+            Console.WriteLine("ArrToList: " + String.Join(",", arrToList));
+            
+            int arrLength = arrToList.Count;
             
            
             if (arrLength % 2 != 0)
             {
-                Console.WriteLine("In If");
-                Console.WriteLine(arr.Length % 2);
-                median = replaceMiddleElement[arrLength / 2];
-                Console.WriteLine("median: " + median);
+                return arrToList.ToArray();
             }
             //if length is even
             else
             {
-                Console.WriteLine("in else");
-                Console.WriteLine("Middle element1: " + replaceMiddleElement[(arrLength / 2) - 1]);
-                Console.WriteLine("Middle element2: " + replaceMiddleElement[arrLength / 2]);
-                //cast integer to float as median is of type float
-                median = (float)(replaceMiddleElement[arrLength / 2] + replaceMiddleElement[(arrLength / 2) - 1]) / 2;
-                Console.WriteLine("Median: " + median);
-                Console.WriteLine("ArrToList: " + String.Join(",", replaceMiddleElement));
-                replaceMiddleElement[(arrLength / 2) - 1] = replaceMiddleElement[(arrLength / 2) - 1] + replaceMiddleElement[arrLength / 2];
-                Console.WriteLine("ArrToList: " + String.Join(",", replaceMiddleElement));
-                replaceMiddleElement.RemoveAt(arrLength / 2);
-                Console.WriteLine("ArrToList: " + String.Join(",", replaceMiddleElement));
+           
+                //Console.WriteLine("Middle element1: " + arrToList[(arrLength / 2) - 1]);
+                //Console.WriteLine("Middle element2: " + arrToList[arrLength / 2]);
+                
+                //Console.WriteLine("ArrToList: " + String.Join(",", arrToList));
+                arrToList[(arrLength / 2) - 1] = arrToList[(arrLength / 2) - 1] + arrToList[arrLength / 2];
+                //Console.WriteLine("ArrToList: " + String.Join(",", arrToList));
+                arrToList.RemoveAt(arrLength / 2);
+                //Console.WriteLine("ArrToList: " + String.Join(",", arrToList));
 
             }
-            return replaceMiddleElement.ToArray();
+            return arrToList.ToArray();
 
         }
 
