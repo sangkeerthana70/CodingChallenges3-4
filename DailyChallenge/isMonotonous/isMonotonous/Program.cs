b9435a7f5dd45730bec1fe7060045f7691941ef2
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-10 23:22:46 -0400

    solved

---
+++ DailyChallenge/isMonotonous/isMonotonous/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isMonotonous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = { 5, 3 , -2 };
+            bool result = isMonotonous(sequence);
+            Console.WriteLine("result " + result);
+        }
+
+        static bool isMonotonous(int[] sequence)
+        {
+            if (sequence.Length == 1)
+            {
+                return true;
+            }
+            bool order; 
+            
+            if(sequence[0] > sequence[1])
+            {
+                order = true;
+            }
+            else
+            {
+                order = false;
+            }
+
+            for(int i = 0; i < sequence.Length - 1; i++)
+            {
+                // if not strictly increasing 
+                if (!(sequence[i] < sequence[i + 1]) && order == false || (sequence[i] == sequence[i + 1]))
+                {
+                    Console.WriteLine("In if");
+
+
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
