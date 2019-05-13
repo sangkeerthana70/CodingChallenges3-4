adf63c06640766b4a8335dc9df959a96dcc19239
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-13 13:05:07 -0400

    Solved

---
+++ Core/AtTheCrossRoads/extraNumber/extraNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extraNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 7;
+            int c = 2;
+            int result = extraNumber(a, b, c);
+            Console.WriteLine("result : " + result);
+        }
+        static int extraNumber(int a, int b, int c)
+        {
+            //int thirdInteger = 0;
+            if (a == b)
+            {
+                //thirdInteger = c;
+                return c;
+            }
+            else if(a == c)
+            {
+                //thirdInteger = b;
+                return b;
+            }
+            else
+            {
+                //thirdInteger = a;
+                return a;
+            }
+            //return thirdInteger;
+        }
+
+    }
+}
