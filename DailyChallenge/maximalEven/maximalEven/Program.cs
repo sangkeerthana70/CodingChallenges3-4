7cebc10227d50bbc087a50a0bc145b6db4387cab
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-15 14:50:24 -0400

    solved

---
+++ DailyChallenge/maximalEven/maximalEven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximalEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 7, 2};
+            maximalEven(inputArray);
+        }
+
+        static int maximalEven(int[] inputArray)
+        {
+            int maxVal = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if(inputArray[i] % 2 == 0)
+                {
+                    if(inputArray[i] > maxVal)
+                    {
+                        maxVal = inputArray[i];
+                    }
+                }
+            }
+            Console.WriteLine("maxVal: " + maxVal);
+            return maxVal;
+        }
+
+    }
+}
