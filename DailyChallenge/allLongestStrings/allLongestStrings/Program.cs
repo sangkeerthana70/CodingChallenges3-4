f7e78c40d65139fac6f9d5ec2c0d710b5012bf2b
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-15 14:31:25 -0400

    working solution

---
+++ DailyChallenge/allLongestStrings/allLongestStrings/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allLongestStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "enyky","benyky","yely","varennyky" };
+            allLongestStrings(inputArray);
+
+        }
+
+        static string[] allLongestStrings(string[] inputArray)
+        {
+
+            List<string> output = new List<string>();
+
+            int maxLength = 0;
+
+            for(int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine("i is: " + i);
+                Console.WriteLine(inputArray[i]);
+                
+                if(inputArray[i].Length > maxLength)
+                {
+                    Console.WriteLine(inputArray[i].Length);
+                    
+                    maxLength = inputArray[i].Length;
+                }
+                    
+            }
+            Console.WriteLine("maxLength: " + maxLength);
+            foreach(string word in inputArray)
+            {
+                if(word.Length == maxLength)
+                {
+                    output.Add(word);
+                }
+            }
+            Console.WriteLine(String.Join(",", output));
+            return output.ToArray();
+        }
+
+    }
+}
