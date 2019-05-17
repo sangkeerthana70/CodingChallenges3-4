6bd0fc2831b280904a0f7eb060deffd611ee4357
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-16 23:21:49 -0400

    Code clean up

--- DailyChallenge/checkFactorial/checkFactorial/Program.cs
+++ DailyChallenge/checkFactorial/checkFactorial/Program.cs
@@ -10,13 +10,21 @@ namespace checkFactorial
     {
         static void Main(string[] args)
         {
-            int n = 120;
+            int n = 25;
             bool result = checkFactorial(n);
-            Console.WriteLine("result: " + result);
-            //int fact = Factorial(n);
-            //Console.WriteLine("fact " + fact);
-           
+            Console.WriteLine("result: " + result);          
         }
+
+        /*
+         * Given an integer n, check if n = k! for some non-negative integer k.
+           For n = 120, the output should be checkFactorial(n) = true.
+           Here 120 = 5!.
+           For n = 25, the output should be
+           checkFactorial(n) = false.
+           Here 4! = 24 < 25 < 120 = 5!.*/
+
+
+
         static bool checkFactorial(int n)
         {
             int k = 1;
