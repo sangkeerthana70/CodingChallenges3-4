59c95e75c08a666d3576c628d9b5e1a8bd5caec6
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-17 14:45:58 -0400

    Initial commit

---
+++ DailyChallenge/subtractionByRegrouping/subtractionByRegrouping/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subtractionByRegrouping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minuend = 2024;
+            int subtrahend = 1234;
+            subtractionByRegrouping(minuend, subtrahend);
+        }
+
+        static int[] subtractionByRegrouping(int minuend, int subtrahend)
+        {
+
+            int[] numbersSubtracted = new int[] { };
+            int mod = minuend % 10;
+            Console.WriteLine("mod : " + mod);
+            int rem = minuend / 10;
+            Console.WriteLine("rem: " + rem);
+            return numbersSubtracted;
+
+        }
+
+    }
+}
