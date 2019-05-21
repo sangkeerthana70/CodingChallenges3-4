bfdcc8c0f0dfef0570f470de3b85bd832ea749aa
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-21 14:00:43 -0400

    passed all test cases

--- DailyChallenge/extractMatrixColumn/extractMatrixColumn/Program.cs
+++ DailyChallenge/extractMatrixColumn/extractMatrixColumn/Program.cs
@@ -18,18 +18,33 @@ namespace extractMatrixColumn
             int[] result = extractMatrixColumn(matrix, column);
             Console.WriteLine("result : " + String.Join(",", result));
         }
+        /*
+        Given a rectangular matrix and an integer column, return an array containing the elements of the
+        columnth column of the given matrix(the leftmost column is the 0th one).
+        For
+
+        matrix = [[1, 1, 1, 2], 
+                    [0, 5, 0, 4], 
+                    [2, 1, 3, 6]]
+        and column = 2, the output should be
+        extractMatrixColumn(matrix, column) = [1, 0, 3].
+        */
+
         static int[] extractMatrixColumn(int[][] matrix, int column)
         {
             List<int> resultArr = new List<int>();
+            // get column length
             int height = matrix.Length;
+            // get individual array in jagged array's length
             int width = matrix[0].Length;
             Console.WriteLine("height = {0}; width = {1}", height, width);
-
+            
             for (int i = 0; i < width; i++)
             {
+               
                 for(int j = 0; j < height; j++)
                 {
-                    Console.WriteLine("j = {0}; i = {1}", j, i);
+                    Console.WriteLine("i = {0}; j = {1}: matrix[i][j] = {2}", i, j, matrix[j][i]);
                     if(i == column)
                     {
                         resultArr.Add(matrix[j][i]);
