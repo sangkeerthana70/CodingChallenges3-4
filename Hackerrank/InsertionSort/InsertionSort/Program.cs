ac8807df886dd91b529b836e9c86e1667f2899c3
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-22 22:30:38 -0400

    Insertion sort

--- Hackerrank/InsertionSort/InsertionSort/Program.cs
+++ Hackerrank/InsertionSort/InsertionSort/Program.cs
@@ -14,37 +14,27 @@ namespace InsertionSort
             int[] arr = new int[] { 2, 4, 6, 8, 3};
             insertionSort1(n, arr);
         }
-        // Complete the insertionSort1 function below.
+        
         // Complete the insertionSort1 function below.
         static void insertionSort1(int n, int[] arr)
         {
             int lastElem = arr[arr.Length-1];
-            //Console.WriteLine("arr length " + arr.Length);                       
+                           
             for (int i = arr.Length - 2; i >= 0; i--)
-            {
-                //Console.WriteLine("arr[i] " + arr[i]);
-                int currIndex = i;
-                //Console.WriteLine("currIndex: " + currIndex);
+            { 
+                int currIndex = i; 
                 if (arr[i] < lastElem)
                 {
                     arr[currIndex + 1] = lastElem;
-                    //Console.WriteLine("inside if after comparing " + String.Join(",", arr));
+  
                     foreach (int num in arr)
                     {
                         Console.Write(" " + num);
-
                     }
-
-
                 }
-
                 else
                 {
-                    //Console.WriteLine("in else");
-
-                    //arr[arr.Length - 1] = arr[i];
                     arr[i + 1] = arr[i];
-                    //Console.WriteLine("inside else " + String.Join(",", arr));
                     foreach (int num in arr)
                     {
                         Console.Write(" " + num);
