9876c427627626bdf73eba4952f54cc8e856840e
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-27 17:45:53 -0400

    working solution

---
+++ DailyChallenge/arrayMaximalDifference/arrayMaximalDifference/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayMaximalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 1, -4, 10, -3, 12 };
+            int result = arrayMaximalDifference(inputArray);
+            Console.WriteLine("result: " + result);
+        }
+
+        static int arrayMaximalDifference(int[] inputArray)
+        {
+            int maxValue = inputArray[0];
+            int maxDiff = 0;
+            for (int j = 0; j < inputArray.Length; j++)
+            {
+                Console.WriteLine("inputARray[j] : " + inputArray[j]);
+                for (int i = j + 1; i < inputArray.Length; i++)
+                {
+                    Console.WriteLine("inputARray[i] : " + inputArray[i]);
+                    Console.WriteLine("maxValue " + maxValue);
+                    maxDiff = Math.Abs(inputArray[j] - inputArray[i]);
+                    Console.WriteLine("maxDiff " + maxDiff);
+                    if (maxValue < maxDiff)
+                    {
+                        maxValue = maxDiff;
+                    }
+
+                }
+                
+            }
+
+            return maxValue;
+        }
+
+    }
+}
