97e15dbbd001a31695c0d67fa71d23481e8475e2
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-27 18:44:07 -0400

    Passed all test cases

--- DailyChallenge/firstMultiple/firstMultiple/Program.cs
+++ DailyChallenge/firstMultiple/firstMultiple/Program.cs
@@ -10,29 +10,39 @@ namespace firstMultiple
     {
         static void Main(string[] args)
         {
-            int[] divisors = { 2, 3, 4 };
-            int start = 13;
+            int[] divisors = { 5, 7, 11};
+            int start = 386;
             int result = firstMultiple(divisors, start);
             Console.WriteLine("result " + result);
 
         }
+
+        /*
+         
+        Find the smallest integer not less than the given limit which is divisible by all integers from the given array.
+
+        Example
+
+        For divisors = [2, 3, 4] and start = 13, the output should be
+        firstMultiple(divisors, start) = 24.
+        */
 
         static int firstMultiple(int[] divisors, int start)
         {
            
             Console.WriteLine("start: " + start);
 
-            while (start <= 400)
+            while (true)
             {
                 bool divisible = true;
                 for (int i = 0; i < divisors.Length; i++)
                 {
-                    Console.WriteLine("i is " + i);
-                    Console.WriteLine("in for loop");
+                    //Console.WriteLine("i is " + i);
+                    //Console.WriteLine("in for loop");
                     
-                    Console.WriteLine("divisors[i] " + divisors[i]);
-                    Console.WriteLine("start in loop: " + start);
-                    Console.WriteLine("start % divisors[i] " + start % divisors[i]);
+                    //Console.WriteLine("divisors[i] " + divisors[i]);
+                    //Console.WriteLine("start in loop: " + start);
+                    //Console.WriteLine("start % divisors[i] " + start % divisors[i]);
                     if ( start % divisors[i] != 0)
                     {
                         divisible = false;
