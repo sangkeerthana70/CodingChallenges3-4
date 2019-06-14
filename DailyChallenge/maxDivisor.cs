ec23cf6b873a91e9a9208db23d446fb392dd2ba6
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-14 00:05:13 -0400

    solved

--- DailyChallenge/maxDivisor.cs
+++ DailyChallenge/maxDivisor.cs
@@ -1,16 +1,26 @@
+/*
+Given a number and a range, find the largest integer within the given range that's divisible by the given number.
+
+Example
+
+For left = 1, right = 10, and divisor = 3, the output should be
+maxDivisor(left, right, divisor) = 9.
+
+The largest integer divisible by 3 in range [1, 10] is 9.
+*/
+
+
 int maxDivisor(int left, int right, int divisor) {
 
-    int Divisor = 0;
+    int maxNumber = 0;
     for(int i = left; i < right; i++){
         if(i % divisor == 0){
-            if(i > Divisor){
-                Divisor = i;
-                Console.WriteLine(i);
-            }
-            
+            if(i > maxNumber){
+                maxNumber = i;                
+            }           
         }
 		
     }
-    return Divisor;
+    return maxNumber;
     
 }
