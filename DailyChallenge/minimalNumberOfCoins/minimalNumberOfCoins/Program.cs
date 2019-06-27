27a67c1f95e8f5bd1270f7501b42f6f419ca793d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-26 23:48:22 -0400

    still working on execution time limit

---
+++ DailyChallenge/minimalNumberOfCoins/minimalNumberOfCoins/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minimalNumberOfCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 28;
+            int[] coins = { 1, 2, 10 };
+            int coinsNeeded = minimalNumberOfCoins(coins, price);
+            Console.WriteLine(coinsNeeded);
+        }
+
+        static int minimalNumberOfCoins(int[] coins, int price)
+        {
+            //Console.WriteLine("coins[0] " + coins[coins.Length - 1]);
+            
+            int remainder = price;
+
+            int coinsNeeded = 0;
+
+            while (remainder != 0)
+            {
+                for (int i = coins.Length - 1; i > 0; i--)
+                {
+                    Console.WriteLine("i : " + i);
+                    if (remainder == 0)
+                    {
+                        return coinsNeeded;
+                    }
+
+                    if (remainder < coins[i])
+                    {
+                        Console.WriteLine("remainder: " + remainder);
+                        Console.WriteLine("coins[i]: " + coins[i]);
+                        continue;
+                    }
+
+                    if (remainder >= coins[i])
+                    {
+                        Console.WriteLine("remainder: " + remainder);
+                        Console.WriteLine("coins[i]: " + coins[i]);
+                        coinsNeeded += price / coins[i];
+                        Console.WriteLine("coinsNeeded: " + coinsNeeded);
+                        remainder = price % coins[i];
+                        Console.WriteLine("remainder: " + remainder);
+                    }
+                }
+            }
+            Console.WriteLine("coinsNeeded: " + coinsNeeded);
+            return coinsNeeded;
+
+        }
+    }
+}
+
