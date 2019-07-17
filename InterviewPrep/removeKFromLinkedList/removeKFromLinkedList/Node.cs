d64bb2e845101aa3c2d2f99783746bed191e6e6d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-16 22:12:10 -0400

    revisiting Linked lists

---
+++ InterviewPrep/removeKFromLinkedList/removeKFromLinkedList/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace removeKFromLinkedList
+{
+    public class Node// represents a singly list node
+    {
+        public int value { get; set; }
+        public Node Next { get; set; }
+
+        //public Node(int val, Node next)
+        //{
+        //    this.value = val;
+        //    this.next = next;
+        //}
+
+
+
+       
+    }
+}
---
+++ InterviewPrep/removeKFromLinkedList/removeKFromLinkedList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace removeKFromLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = new List<int> { 3, 1, 2, 3, 4, 5 };
+            int k = 3;
+            Node head = CreateLinkedList(input);
+            Console.WriteLine("head " + head.value);
+            
+
+        }
+
+        static Node CreateLinkedList(List<int> integerList)
+        {
+            Node head = new Node();
+
+            var currNode = head;
+
+            for(int i = 0; i < integerList.Count; i++)
+            {
+                currNode.value = integerList[i];
+                Console.WriteLine("currNode.value " + currNode.value);
+
+                if(i < integerList.Count - 1)
+                {
+                    currNode.Next = new Node();
+                    currNode = currNode.Next;
+                }
+            }
+            return head;
+        }
+    }
+}
