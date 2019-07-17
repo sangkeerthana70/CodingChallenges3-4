508f384fc6a951ce92e2bb69e7d982aee9303821
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-16 23:27:51 -0400

    creating a linked list from a list of integers and printing them by traversing the Linked list

--- InterviewPrep/removeKFromLinkedList/removeKFromLinkedList/Program.cs
+++ InterviewPrep/removeKFromLinkedList/removeKFromLinkedList/Program.cs
@@ -13,9 +13,14 @@ namespace removeKFromLinkedList
             List<int> input = new List<int> { 3, 1, 2, 3, 4, 5 };
             int k = 3;
             Node head = CreateLinkedList(input);
-            Console.WriteLine("head " + head.value);
-            
+            //Console.WriteLine("head " + head.value);
 
+            //PrintElementsInLinkedList(head);
+
+            Node Head = RemoveKFromList(head, k);
+            Console.WriteLine("newHead " + Head);
+
+            PrintElementsInLinkedList(Head);
         }
 
         static Node CreateLinkedList(List<int> integerList)
@@ -35,6 +40,35 @@ namespace removeKFromLinkedList
                     currNode = currNode.Next;
                 }
             }
+            return head;
+        }
+
+        static void PrintElementsInLinkedList(Node head)
+        {
+            var currNode = head;
+            
+            while (currNode != null)
+            {
+
+                Console.WriteLine("element in Linked list " + currNode.value);
+                currNode = currNode.Next;
+
+            }
+        }
+
+        static Node RemoveKFromList(Node head, int k)
+        {
+            Console.WriteLine("inside remove method");
+            if (head == null)
+                return head;
+
+            var currNode = head;
+            // if first node is equal to K
+            if(currNode.value == k)
+            {
+                head = currNode.Next;
+            }
+           
             return head;
         }
     }
