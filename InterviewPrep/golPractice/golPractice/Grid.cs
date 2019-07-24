d42bbb84b1d336c2693b21829dd46a531f881389
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-23 22:53:20 -0400

    practice on gol

---
+++ InterviewPrep/golPractice/golPractice/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golPractice
+{
+    public class Grid
+    {
+        public bool[,] grid { get; set; }
+
+        public Grid(int rows, int cols)
+        {
+            this.grid = new bool[rows, cols];
+
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
