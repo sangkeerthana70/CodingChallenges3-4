2bc3018b9ebca0ddda84c9a96982037cd84c0b85
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-23 22:39:03 -0400

    Solved

--- DailyChallenge/areSimilarNumbers.cs
+++ DailyChallenge/areSimilarNumbers.cs
@@ -1,12 +1,15 @@
+
+/*
+Call two integers a and b similar if divisor divides both a and b or if it doesn't divide either.
+
+Given integers a, b and divisor, check if a and b are similar.
+*/
+
+
 bool areSimilarNumbers(int a, int b, int divisor) {
 
-    if((a % divisor == 0) && (b % divisor == 0))
+    if (((a % divisor == 0) && (b % divisor == 0)) || ((a % divisor != 0) && (b % divisor != 0)))
         return true;
-    else if(a % divisor == 0 || b % divisor != 0)
-        return true;
-    
-   // else if(a % divisor != 0 || b % divisor == 0)
-        //return true;
-
-    return false;
+    else
+        return false;
 }
