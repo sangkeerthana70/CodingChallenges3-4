24af383acd8953c64805dafaa5ded4509d39de37
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-30 12:47:06 -0400

    Approach 2 and three implemented to find the difference in two dates

--- dateCheckChallenge/dateCheckChallenge/Program.cs
+++ dateCheckChallenge/dateCheckChallenge/Program.cs
@@ -17,27 +17,45 @@ namespace dateCheckChallenge
         }
         static decimal ConvertToDecimal(DateTime input)
         {
-            decimal decimalvalOfMonth;
+            /* Method-1
+            decimal decimalValOfMonth;
             decimal decimalValOfYear;
-            Console.WriteLine(input.Year);
-            Console.WriteLine(input.Month);
-            Console.WriteLine(input.Date);
-
+     
             DateTime currentdate = DateTime.Now;
-            Console.WriteLine(currentdate.Year);
-
-
-            //decimalval = Convert.ToDecimal(currentdate - input);
             decimalValOfYear = (currentdate.Year - input.Year);
             
 
            
-            decimalvalOfMonth = currentdate.Month - input.Month;
+            decimalValOfMonth = currentdate.Month - input.Month;
 
-
-            return Convert.ToDecimal(decimalValOfYear + "." + decimalvalOfMonth);
+            // convert the concatenated string to decimal
+            return Convert.ToDecimal(decimalValOfYear + "." + decimalValOfMonth);
+            */
+            //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            /*Method-2 using TimeSpan
+            DateTime currentDate = DateTime.Now;
+            TimeSpan difference = currentDate - input;
 
             
+            Console.WriteLine("   {0,-35} {1,20}", "Total Number of Days:", difference.TotalDays/365);
+
+            //use Math.Round to get two decimal values in the decimal          
+            return Math.Round(Convert.ToDecimal(difference.TotalDays / 365), 2);
+            */
+            //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+            //Method-3 
+            //if using milliseconds to calculate the difference between two years
+            //millseconds/1000/60/60/24/365
+            //convert milliseconds to seconds to minute to hour to days.
+
+            DateTime currentDate = DateTime.Now;
+            TimeSpan difference = currentDate - input;
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Total Number of Milliseconds:", difference.TotalMilliseconds);
+            double milliseconds = difference.TotalMilliseconds;
+            return Math.Round(Convert.ToDecimal(milliseconds/1000/60/60/24/365), 2);
         }
+
+
     }
 }
