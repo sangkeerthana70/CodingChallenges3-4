67f3f5e773de6fbdf0e5ac2ac9d2d975ab60112a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-30 11:45:46 -0400

    method - 1 to solve the difference in two date times

---
+++ dateCheckChallenge/dateCheckChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateCheckChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime input = new DateTime(2012, 1, 1);
+            decimal result = ConvertToDecimal(input);
+            Console.WriteLine("result :" + result);
+
+        }
+        static decimal ConvertToDecimal(DateTime input)
+        {
+            decimal decimalvalOfMonth;
+            decimal decimalValOfYear;
+            Console.WriteLine(input.Year);
+            Console.WriteLine(input.Month);
+            Console.WriteLine(input.Date);
+
+            DateTime currentdate = DateTime.Now;
+            Console.WriteLine(currentdate.Year);
+
+
+            //decimalval = Convert.ToDecimal(currentdate - input);
+            decimalValOfYear = (currentdate.Year - input.Year);
+            
+
+           
+            decimalvalOfMonth = currentdate.Month - input.Month;
+
+
+            return Convert.ToDecimal(decimalValOfYear + "." + decimalvalOfMonth);
+
+            
+        }
+    }
+}
