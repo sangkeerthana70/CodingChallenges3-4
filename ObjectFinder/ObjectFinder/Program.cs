4d9e9e3d498c5c766ffec0d870d45860e31f5f9c
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-08-01 23:50:13 -0400

    Initial commit

---
+++ ObjectFinder/ObjectFinder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Object Finder
+
+            Given an array of objects All with the same keys and A key and value. Return the index of the object that has that key and value.
+
+            ex:[{name:Cliff age:32} {name:John age:25} {name:Andy age:32} {name:Mary age:21} {name:Sally age:25}]
+
+            Age:25 should return output as 1.
+            if using array or list of dictionary return index of dictionary, try using bracket notation[""]
+
+            ==================================================================================
+            Additional challenge
+            1.Return all integers that match the index in an array[1,4]
+
+            2.With the key given find the object where any key matches
+            Ex:{a:Foo b:bar} {a:duh b:gif} {a:hi b:Foo} {
+            Key:Foo return [0,2]
+            */
+
+            var list = new List<KeyValuePair<string, int>>();
+            list.Add(new KeyValuePair<string, int>("Cliff", 32));
+            list.Add(new KeyValuePair<string, int>("John", 25));
+            list.Add(new KeyValuePair<string, int>("Andy", 32));
+            list.Add(new KeyValuePair<string, int>("Mary", 21));
+            list.Add(new KeyValuePair<string, int>("Sally", 25));
+
+            foreach(var keyvalue in list)
+            {
+                Console.WriteLine("keyVal " + keyvalue);
+               
+            }
+
+            GetMatchingIndex(list);
+
+
+
+        }
+
+        static int GetMatchingIndex(List<KeyValuePair<string, int>> data)
+        {
+            foreach(var dataSet in data)
+            {
+                
+            }
+            
+        }
+        
+    }
+}
