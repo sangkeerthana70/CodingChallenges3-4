b22366e5e3ec2d94435a8d814380ceb242fb4899
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-08-15 23:51:43 -0400

    initial commit

---
+++ DailyChallenge/Euler'sTotientFunction/Euler'sTotientFunction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_sTotientFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int result = eulersTotientFunction(n);
+
+        }
+
+        static int eulersTotientFunction(int n)
+        {
+
+
+        }
+
+        static int[] findFactors(int number)
+        {
+            List<int> factors = new List<int>();
+            for (int i = 2; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    factors.Add(i);
+                }
+            }
+
+            return factors.ToArray();
+        }
+    }
+}
